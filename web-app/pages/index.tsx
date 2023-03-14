10a590e4c7c192819d8f0f041239330911c41b78
Ivan Bulavko <Ivan_Bulavko@epam.com>
2023-03-14 11:43:12 +0200

    Project initialization

---
+++ web-app/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>RTL NFT</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div>Main page</div>
+      </main>
+    </>
+  );
+}
